0653b901a70afc2e703d8c6b74a18b96ae3706bb
Rudransh Raj Srivastava <rudranshrajsrivastava95@gmail.com>
2026-02-10 11:54:19 +0530

    'Improved'

---
+++ AddFive.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.26;
+
+import {SimpleStorage} from "./SimpleStorage.sol";
+
+contract AddFive is SimpleStorage{
+    // Inheritence 
+    // The class AddFive is the child class of the parent class SimpleStorage
+    
+}
